76fbd019cc88808cdb231d32956370ed5087af45
CommanderCody01 <qy624@nyu.edu>
2023-04-05 12:39:37 -0400

    your commit message

---
+++ src/app.mjs
@@ -0,0 +1,176 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import sanitize from 'mongo-sanitize';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import bcrypt from 'bcryptjs';
+import session from 'express-session';
+import './db.mjs';
+import {startAuthenticatedSession, endAuthenticatedSession} from './auth.mjs';
+
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'hbs');
+
+app.use(session({
+    secret: 'add session secret here!',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+const User = mongoose.model('User');
+const Game = mongoose.model('Game');
+const Review = mongoose.model('Review');
+
+const authRequired = (req, res, next) => {
+    if(!req.session.user) {
+      req.session.redirectPath = req.path;
+      res.redirect('/login'); 
+    } else {
+      next();
+    }
+  };
+  
+app.use((req, res, next) => {
+    // can use {{user}} in your template
+    res.locals.user = req.session.user;
+    next();
+});
+
+
+app.get("/",(req, res) =>{
+
+    res.render('index', {user: req.session.user});
+});
+
+app.get("/user/:slug",authRequired, async(req, res) =>{
+
+    const user = await User.findOne({username: req.params.slug}).exec();
+    
+    console.log(user);
+    res.render('user',{data: user.gameLists});
+
+});
+
+app.post("/user/:slug",authRequired, async(req, res) =>{
+
+    const user = await User.findOne({username: req.params.slug}).exec();
+    const s = '/user/' + user.username;
+    const gn = req.body.gameName;
+    const gm = await Game.findOne({gameName: gn}).exec();
+
+    if(!gm){                      // 只有没找到game才能add game 进DB
+        const g = new Game({
+            gameName: req.body.gameName,
+            company: req.body.company,
+            type: req.body.type,
+            year: req.body.year,
+        });
+        g.save().then( async(saved) => {
+            const list = user.gameLists;
+            list.push(g);
+            await User.updateOne({username: req.params.slug},{$set: {gameLists: list}}).exec();
+            res.redirect(s);
+        } );
+    }
+    else{
+        const list = user.gameLists;
+        list.push(gm);
+        await User.updateOne({username: req.params.slug},{$set: {gameLists: list}}).exec();
+        res.redirect(s);
+    }
+
+    
+
+    console.log(user);
+    // if( user.gameLists === null){
+    //     user.gameLists = [];
+    // }
+
+
+
+});
+
+app.get("/register",(req, res) =>{
+    res.render('register')
+});
+
+app.post('/register', async (req, res) => {
+    const username = sanitize(req.body.username);
+    const password = sanitize(req.body.password);
+    const email = sanitize(req.body.email);
+  
+    try {
+
+      const existingUser = await User.findOne({username}).exec();
+      if(existingUser){
+        res.render('register', {message: "could not register"});
+      }
+      else{
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        const user = new User({
+          username: username,
+          password: hash,
+          email: email
+        })
+        const savedUser = await user.save();
+        await startAuthenticatedSession(req, savedUser);
+        res.redirect("/");
+      }
+  
+    } catch (err) {
+      if(err instanceof mongoose.Error.ValidationError) {
+        res.render('register', {message: err.message});
+      } else {
+        throw err;
+      }
+    }
+  });
+
+app.post('/logout', async (req, res) => {
+    await endAuthenticatedSession(req);
+    res.redirect('/');
+});
+
+app.get("/login",(req, res) =>{
+    res.render('login')
+});
+
+app.post('/login', async (req, res) => {
+    const username = sanitize(req.body.username);
+    const password = sanitize(req.body.password);
+  
+    try {
+
+      const existingUser = await User.findOne({username}).exec();
+
+      if(existingUser){
+        // console.log(bcrypt.compareSync(password , existingUser.password));
+        if(bcrypt.compareSync(password , existingUser.password)){
+          await startAuthenticatedSession(req, existingUser);
+          res.redirect("/");
+        }
+        else{
+          res.render('login', {message: "Wrong password"});
+        }
+  
+      }else{
+        res.render('login', {message: "Did not find user"});
+      }
+  
+  
+    } catch (err) {
+      if(err instanceof mongoose.Error.ValidationError) {
+        res.render('login', {message: err.message});
+      } else {
+        throw err;
+      }
+    }
+  });
+
+app.listen(process.env.PORT || 3000);
---
+++ src/db.mjs
@@ -0,0 +1,76 @@
+import mongoose from 'mongoose'
+import slug from 'mongoose-slug-updater';
+
+
+
+console.log('Waiting for connection to database...')
+try {
+  await mongoose.connect('mongodb://localhost/finalTest', {useNewUrlParser: true});
+  console.log('Successfully connected to database.')
+} catch (err) {
+  console.log('ERROR: ', err);
+}
+
+mongoose.plugin(slug);
+
+
+const reviewScheme = new mongoose.Schema({
+  reviewManName: {type: String, required: true, minLength: 1},         // gameName 是unique的
+  game: {type: String, required: true},
+  description: {type: String, required: true},
+  slug: {type: String, slug: ["reviewManName","game"], unique: true}
+},{timestamps: true}
+);
+
+const gameSchema = new mongoose.Schema({
+  gameName: {type: String, required: true, minLength: 1},         // gameName 是unique的
+  company: {type: String, required: true},
+  type: {type: String, },
+  year: {type: String},
+  reviews: [reviewScheme]
+});
+
+const UserSchema = new mongoose.Schema({
+  username: {type: String, required: true, minLength: 1, maxLength: 20},         // userName 是unique的
+  password: {type: String, required: true, minLength: 8},
+  email: {type: String, required: true},
+  gameLists: [gameSchema]
+});
+
+
+
+
+
+mongoose.model('User', UserSchema);
+mongoose.model('Game', gameSchema);
+mongoose.model('Review', reviewScheme);
+
+// // OPTIONAL: modify the connection code below if
+// // using mongodb authentication
+// const mongooseOpts = {
+//   useNewUrlParser: true,  
+//   useUnifiedTopology: true
+// };
+
+// // mongoose.connect('mongodb://localhost/hw05', mongooseOpts, (err) => {
+// //   if (err) {
+// //     console.log(err);
+// //   } else {
+// //     console.log('connected to database'); 
+// //   }
+// // });
+
+// // TODO: create schema and register models
+
+// const Review = new mongoose.Schema({
+//   courseNumber: String,
+//   courseName: String,
+//   semester: String,
+//   year: Number,
+//   professor: String,
+//   review: String
+// });
+
+// mongoose.model("Review",Review);
+
+// mongoose.connect('mongodb://localhost/hw05');
